fe545054b18a67a342c3a1c48ffb34ddddc3038b
Jaeduk Han <jdhan@eecs.berkeley.edu>
2017-02-11 17:18:47 -0800

    subradix capdac

--- generators/adc_sar_capdac_simple_layout_generator.py
+++ generators/adc_sar_capdac_simple_layout_generator.py
@@ -29,19 +29,10 @@ import laygo
 import numpy as np
 import os
 
-def generate_capdac(laygen, objectname_pfix, placement_grid, routing_grid_m6m7,
+def generate_capdac_simple(laygen, objectname_pfix, placement_grid, routing_grid_m6m7,
                     devname_cap_body='momcap_center', devname_cap_dmy='momcap_dmy', devname_cap_space='momcap_space',
                     devname_cap_boundary='momcap_boundary',
-                    m_unit=1,
-                    num_bits_vertical=5,
-                    m_vertical=np.array([1,2,4,8,16,32]),
-                    num_bits_horizontal=2,
-                    m_horizontal=np.array([64, 64]),
-                    num_space_left=1,
-                    num_space_right=1,
-                    num_space_top=1,
-                    num_space_bottom=1,
-                    origin=np.array([0, 0])):
+                    m_unit=1, num_bits_vertical=5, num_bits_horizontal=2, num_col_space_right=1, origin=np.array([0, 0])):
 
     """
     Generate a capdac array
@@ -68,14 +59,14 @@ def generate_capdac(laygen, objectname_pfix, placement_grid, routing_grid_m6m7,
     # placement
     #left boundaries
     ibndbl0 = laygen.place(name = "I" + objectname_pfix + "BNDBL0", templatename = devname_cap_boundary,
-                           gridname = pg, xy=origin, shape=np.array([num_space_left, num_space_bottom]))
+                           gridname = pg, xy=origin)
     ibndl0 = laygen.relplace(name = "I" + objectname_pfix + "BNDL0", templatename = devname_cap_boundary,
-                             gridname = pg, refinstname=ibndbl0.name, shape=np.array([num_space_left, m*2**num_bits_vertical]), direction='top')
+                             gridname = pg, refinstname=ibndbl0.name, shape=np.array([1, m*2**num_bits_vertical]), direction='top')
     ibndtl0 = laygen.relplace(name = "I" + objectname_pfix + "BNDTL0", templatename = devname_cap_boundary,
-                              gridname = pg, refinstname=ibndl0.name, shape=np.array([num_space_left, num_space_top]), direction='top')
+                              gridname = pg, refinstname=ibndl0.name, direction='top')
     ibndb=[]
     ibndb.append(laygen.relplace(name="I" + objectname_pfix + 'BNDB0', templatename=devname_cap_boundary,
-                                 gridname=pg, refinstname=ibndbl0.name, shape=np.array([1, num_space_bottom])))
+                                 gridname=pg, refinstname=ibndbl0.name))
     #c0 -single, unit sized cap (will be connected to ground)
     ic0 = laygen.relplace(name = "I" + objectname_pfix + "C0", templatename = devname_cap_body,
                           gridname = pg, refinstname=ibndb[0].name, shape=np.array([1, m]), direction='top')
@@ -83,41 +74,36 @@ def generate_capdac(laygen, objectname_pfix, placement_grid, routing_grid_m6m7,
     ivdac=[]
     for i in range(num_bits_vertical):
         if i==0: refin=ic0.name
-        #ivdac.append(laygen.relplace(name="I" + objectname_pfix + 'VDAC' + str(i), templatename=devname_cap_body,
-        #                             gridname=pg, refinstname=refin, shape=np.array([1, m*2**i]), direction='top')) #radix2
+        else: refin=ivdac[i-1].name
         ivdac.append(laygen.relplace(name="I" + objectname_pfix + 'VDAC' + str(i), templatename=devname_cap_body,
-                                     gridname=pg, refinstname=refin, shape=np.array([1, m*m_vertical[i]]), direction='top'))
-        refin=ivdac[i].name
-        if m_vertical[i]<2**i:
-            idmy=laygen.relplace(name="I" + objectname_pfix + 'VDACDMY' + str(i), templatename=devname_cap_dmy,
-                                     gridname=pg, refinstname=refin, shape=np.array([1, m*(2**i-m_vertical[i])]), direction='top')
-            refin=idmy.name
+                                     gridname=pg, refinstname=refin, shape=np.array([1, m*2**i]), direction='top'))
     ibndt=[]
     ibndt.append(laygen.relplace(name="I" + objectname_pfix + 'BNDT0', templatename=devname_cap_boundary,
-                                 gridname=pg, refinstname=refin, shape=np.array([1, num_space_top]), direction='top'))
+                                 gridname=pg, refinstname=ivdac[-1].name, direction='top'))
     #hdac
     ihdac=[]
     for i in range(num_bits_horizontal):
         ibndb.append(laygen.relplace(name="I" + objectname_pfix + 'BNDB' + str(i+1), templatename=devname_cap_boundary,
-                                     gridname=pg, refinstname=ibndb[-1].name, shape=np.array([2**i, num_space_bottom])))
-        #ihdac.append(laygen.relplace(name="I" + objectname_pfix + 'HDAC' + str(i), templatename=devname_cap_body,
-        #                             gridname=pg, refinstname=ibndb[-1].name, shape=np.array([2**i, m*2**num_bits_vertical]), direction='top')) #radix2
+                                     gridname=pg, refinstname=ibndb[-1].name, shape=np.array([2**i, 1])))
         ihdac.append(laygen.relplace(name="I" + objectname_pfix + 'HDAC' + str(i), templatename=devname_cap_body,
-                                     gridname=pg, refinstname=ibndb[-1].name, shape=np.array([2**i, m*m_horizontal[i]]), direction='top'))
-        refin=ihdac[i].name
-        if m_horizontal[i]<m*2**num_bits_vertical:
-            idmy=laygen.relplace(name="I" + objectname_pfix + 'HDACDMY' + str(i), templatename=devname_cap_dmy,
-                                     gridname=pg, refinstname=refin, shape=np.array([2**i, m*(m*2**num_bits_vertical-m_horizontal[i])]), direction='top')
-            refin=idmy.name
+                                     gridname=pg, refinstname=ibndb[-1].name, shape=np.array([2**i, m*2**num_bits_vertical]), direction='top'))
         ibndt.append(laygen.relplace(name="I" + objectname_pfix + 'BNDT' + str(i+1), templatename=devname_cap_boundary,
-                                     gridname=pg, refinstname=refin, shape=np.array([2**i, num_space_top]), direction='top'))
+                                     gridname=pg, refinstname=ihdac[-1].name, shape=np.array([2**i, 1]), direction='top'))
+    #right dummies
+    #right boundaries
+    idmybr0 =laygen.relplace(name = "I" + objectname_pfix + "BNDBDMYR0", templatename = devname_cap_boundary,
+                             gridname = pg, refinstname=ibndb[-1].name, shape=np.array([num_col_space_right, 1]))
+    idmyr0 = laygen.relplace(name = "I" + objectname_pfix + "SPACER0", templatename = devname_cap_space,
+                             gridname = pg, refinstname=idmybr0.name, shape=np.array([num_col_space_right, m * 2 ** num_bits_vertical]), direction='top')
+    idmytr0 = laygen.relplace(name = "I" + objectname_pfix + "BNDTDMYR0", templatename = devname_cap_boundary,
+                              gridname = pg, refinstname=idmyr0.name, shape=np.array([num_col_space_right, 1]), direction='top')
     #right boundaries
     ibndbr0 =laygen.relplace(name = "I" + objectname_pfix + "BNDBR0", templatename = devname_cap_boundary,
-                             gridname = pg, refinstname=ibndb[-1].name, shape=np.array([num_space_right, num_space_bottom]))
+                             gridname = pg, refinstname=idmybr0.name)
     ibndr0 = laygen.relplace(name = "I" + objectname_pfix + "BNDR0", templatename = devname_cap_boundary,
-                             gridname = pg, refinstname=ibndbr0.name, shape=np.array([num_space_right, m*2**num_bits_vertical]), direction='top')
+                             gridname = pg, refinstname=ibndbr0.name, shape=np.array([1, m*2**num_bits_vertical]), direction='top')
     ibndtr0 = laygen.relplace(name = "I" + objectname_pfix + "BNDTR0", templatename = devname_cap_boundary,
-                              gridname = pg, refinstname=ibndr0.name, shape=np.array([num_space_right, num_space_top]), direction='top')
+                              gridname = pg, refinstname=ibndr0.name, direction='top')
     #reference route coordinate
     y0 = 1
     #c0 route
@@ -129,26 +115,22 @@ def generate_capdac(laygen, objectname_pfix, placement_grid, routing_grid_m6m7,
     #bottom route
     rbot=[]
     for i, c in enumerate(ivdac):
-        #c_bot_xy = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([0, m*2**i-1]))[0] #radix2
-        c_bot_xy = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([0, m * m_vertical[i] - 1]))[0]
+        c_bot_xy = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([0, m*2**i-1]))[0]
         c_bot_xy2 = laygen.get_template_pin_coord(c.cellname, 'BOTTOM', rg_m6m7)[0]
         rbot.append(laygen.route(None, laygen.layers['metal'][7], xy0=c_bot_xy + np.array([2*i+2, 0]), xy1=np.array([0, y0]), gridname0=rg_m6m7, direction='y'))
-        #for j in range(m*2**i): #radix2
-        for j in range(m * m_vertical[i]):
+        for j in range(m*2**i):
             laygen.via(None, c_bot_xy2 + np.array([2*i+2, 0]), refinstname=c.name, refinstindex=np.array([0, j]), gridname=rg_m6m7)
     for i, c in enumerate(ihdac):
-        #c_bot_xy = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([0, m*2**num_bits_vertical-1]))[0]
-        c_bot_xy = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([0, m*m_horizontal[i]-1]))[0]
+        c_bot_xy = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([0, m*2**num_bits_vertical-1]))[0]
         c_bot_xy2 = laygen.get_template_pin_coord(c.cellname, 'BOTTOM', rg_m6m7)[0]
-        #rbot.append(laygen.route(None, laygen.layers['metal'][7], xy0=c_bot_xy + np.array([0, 0]), xy1=np.array([0, y0]), gridname0=rg_m6m7, direction='y'))
-        #for j in range(m*2**num_bits_vertical):
-        #for j in range(m*m_horizontal[i]):
-        #    laygen.via(None, c_bot_xy2 + np.array([0, 0]), refinstname=c.name, refinstindex=np.array([0, j]), gridname=rg_m6m7)
+        rbot.append(laygen.route(None, laygen.layers['metal'][7], xy0=c_bot_xy + np.array([0, 0]), xy1=np.array([0, y0]), gridname0=rg_m6m7, direction='y'))
+        for j in range(m*2**num_bits_vertical):
+            laygen.via(None, c_bot_xy2 + np.array([0, 0]), refinstname=c.name, refinstindex=np.array([0, j]), gridname=rg_m6m7)
         #parallel connections
         for k in range(2**i):
-            c_bot_xy3 = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([k, m*m_horizontal[i]-1]))[0]
+            c_bot_xy3 = laygen.get_inst_pin_coord(c.name, 'BOTTOM', rg_m6m7, index=np.array([k, m*2**num_bits_vertical-1]))[0]
             laygen.route(None, laygen.layers['metal'][7], xy0=c_bot_xy3 + np.array([0, 0]), xy1=np.array([0, y0+2-2]), gridname0=rg_m6m7, direction='y')
-            for j in range(m*m_horizontal[i]):
+            for j in range(m*2**num_bits_vertical):
                 laygen.via(None, c_bot_xy2 + np.array([0, 0]), refinstname=c.name, refinstindex=np.array([k, j]), gridname=rg_m6m7)
         #col shorts
         if not i==0:
@@ -164,8 +146,7 @@ def generate_capdac(laygen, objectname_pfix, placement_grid, routing_grid_m6m7,
         laygen.create_boundary_pin_form_rect(r, rg_m6m7, "I<"+str(i)+">", laygen.layers['pin'][7], size=4, direction='bottom')
     cnt=0
     for i, c in enumerate(ivdac):
-        #for j in range(m * 2 ** i): #radix2
-        for j in range(m * m_vertical[i]):
+        for j in range(m * 2 ** i):
             c_top_xy = laygen.get_inst_pin_coord(c.name, 'TOP', rg_m6m7, index=np.array([0, j]))
             rtop = laygen.route(None, laygen.layers['metal'][6], xy0=c_top_xy[0], xy1=c_top_xy[1], gridname0=rg_m6m7)
             laygen.create_boundary_pin_form_rect(rtop, rg_m6m7, "O"+str(cnt), laygen.layers['pin'][6], size=4, direction='left', netname="O")
@@ -212,31 +193,18 @@ if __name__ == '__main__':
     mycell_list = []
     #cap dac generation
     m_unit=1
-    num_bits_vertical=6
+    num_bits_vertical=5
     num_bits_horizontal=2
-    num_space_top = 4
-    num_space_bottom = 4
-    num_space_left = 1
-    num_space_right = 2
-    cellname='capdac_8b'
+    num_col_dmy_right=1
+    cellname='capdac_7b'
     print(cellname+" generating")
     mycell_list.append(cellname)
     laygen.add_cell(cellname)
     laygen.sel_cell(cellname)
-    #routing grid used as placement grid
-    generate_capdac(laygen, objectname_pfix='CDAC0', placement_grid=rg_m6m7, routing_grid_m6m7=rg_m6m7,
-                    devname_cap_body='momcap_center_1x', devname_cap_dmy='momcap_dmy_1x', devname_cap_space='momcap_space_1x',
-                    devname_cap_boundary='momcap_boundary_1x',
-                    m_unit=m_unit,
-                    num_bits_vertical=num_bits_vertical,
-                    m_vertical=np.array([1,2,4,8,16,30]),
-                    num_bits_horizontal=num_bits_horizontal,
-                    m_horizontal=np.array([56, 53]),
-                    num_space_left=num_space_left,
-                    num_space_right=num_space_right,
-                    num_space_top=num_space_top,
-                    num_space_bottom=num_space_bottom,
-                    origin=np.array([0, 0]))
+    generate_capdac_simple(laygen, objectname_pfix='CDAC0', placement_grid=pg, routing_grid_m6m7=rg_m6m7,
+                    devname_cap_body='momcap_center', devname_cap_dmy='momcap_dmy', devname_cap_space='momcap_space',
+                    devname_cap_boundary='momcap_boundary',
+                    m_unit=m_unit, num_bits_vertical=num_bits_vertical, num_bits_horizontal=num_bits_horizontal, num_col_space_right=num_col_dmy_right, origin=np.array([0, 0]))
 
     #cap_dim_vert_x = np.ones()
     #cap_dim_vert_y = np.array([1, 2, 4, 8, 16, 32])
