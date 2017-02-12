59316886694ea936f2d2c6bebdac3e32ff6953c3
Jaeduk Han <jdhan@bwrcrdsl-6.EECS.Berkeley.EDU>
2017-02-11 23:01:49 -0800

    capdac redundancy

--- generators/adc_sar_dsn_system.py
+++ generators/adc_sar_dsn_system.py
@@ -2,7 +2,7 @@
 
 import pprint
 
-import bag
+#import bag
 import numpy as np
 import yaml
 from math import log10
@@ -10,26 +10,36 @@ from math import log10
 #high level spec
 T=300+100                           #temperature
 kT=1.38e-23*T
-vin=0.3                             #input range
-nbit=8                              #number of bits
+v_in=0.3                             #input range
+n_bit=9                              #number of bits
 fsamp=9.6e9                         #effective sampling rate
-c_unit_min=0.2e-15                  #minimum cap size from matching or min device size requirements
-vbit=vin/2**nbit                    #1 bit step
-nbit_samp_noise=0.25                #sampling noise level in bits
-nbit_samp_settle=0.25               #sampling settling error level in bits
-nbit_comp_noise=0.25                #comparator noise level in bits
-#nbit_comp_kickback=0.5             #comparator kickback level in bits
-v_samp_noise=vbit*nbit_samp_noise   #sampling noise
-v_comp_noise=vbit*nbit_comp_noise   #comparator noise
-samp_settle_error=nbit_samp_settle/nbit #settling error
+c_unit=0.205e-15                    #minimum cap size (set by process and template)
+c_m=1                               #multiplier of unit cap for LSB 
+c_delta=0.2                         #Max. cap mismatch ratio for c_unit
+n_bit_samp_noise=0.5                 #Max. sampling noise level in bits
+n_bit_samp_settle=0.5                #Max. sampling settling error level in bits
+n_bit_comp_noise=0.5                 #Max. comparator noise level in bits
+#n_bit_comp_kickback=0.5             #comparator kickback level in bits
+v_bit=v_in/2**n_bit                    #1 bit step
+c_samp=c_unit_min*2**n_bit           #sampling cap from c_unit
+
+print("[Top level parameters]")
+print("v_bit:"+str(v_bit*1e3)+"mV")
+print("c_samp:"+str(c_samp*1e15)+"fF")
+#noise calculaton
+v_samp_noise=v_bit*n_bit_samp_noise   #sampling noise
+v_comp_noise=v_bit*n_bit_comp_noise   #comparator noise
+c_samp_noise=kT/v_samp_noise**2     #sampling cap from noise requirement
+c_unit_noise=c_samp_noise/2**n_bit   #unit cap calculated from noise requirement
+print("[Noise analysis]")
+print("v_samp_noise:"+str(v_samp_noise*1e3)+"mV")
+print("v_comp_noise:"+str(v_comp_noise*1e3)+"mV")
+print("c_samp_noise:"+str(c_samp_noise*1e15)+"fF")
+
+samp_settle_error=n_bit_samp_settle/n_bit #settling error
 samp_ntau=2.3*log10(1/samp_settle_error) #ntau of sampling network
 f_samp_bw=samp_ntau*fsamp/6.28      #sampling bandwidth
-c_samp_noise=kT/v_samp_noise**2     #sampling cap from noise requirement
-c_unit_noise=c_samp_noise/2**nbit   #unit cap from noise requirement
-c_samp_min=c_unit_min*2**nbit       #sampling cap from minimum device requirement
-c_samp=max(c_samp_noise, c_samp_min) #sampling cap
 r_samp=1/f_samp_bw/c_samp           #sampling network switch resistance
 
-print("ADC top level parameters")
 
 
